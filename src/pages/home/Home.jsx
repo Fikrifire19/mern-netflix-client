d0ac4f61434740d4343c2ad9b24347e384d1f0c3
Fikri <muhamadfikri64@yahoo.co.id>
2021-08-12 21:39:40 +0700

    Step 1 - Fixed Navbar Components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     return (
         <div className="home">
             <Navbar />
+            <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80" alt="profile picture" />
         </div>
     )
 };
