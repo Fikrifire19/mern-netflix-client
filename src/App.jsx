62168480142a3112067ef6f49b67a4eb26581723
Fikri <muhamadfikri64@yahoo.co.id>
2021-08-22 21:17:35 +0700

    Step 6 - Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
-    <Watch />
+    <Register />
   );
 }
 
